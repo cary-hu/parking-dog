531b76be4cf6ef2af84bd0713dbd6664f22a9dd5
Cary <305997735@qq.com>
2023-06-10 22:30:40 +0800

    fix: waning in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import * as VueRouter from 'vue-router'
 
 const routes = [
-  { path: '/', component: import('@/pages/Home.vue') },
-  { path: '/parking-lot', component: import('@/pages/ParkingLot.vue') },
-  { path: '/parking', component: import('@/pages/ParkingInfo.vue') },
+  { path: '/', component: () => import('@/pages/Home.vue') },
+  { path: '/parking-lot', component: () => import('@/pages/ParkingLot.vue') },
+  { path: '/parking', component: () => import('@/pages/ParkingInfo.vue') },
 ]
 
 const router = VueRouter.createRouter({

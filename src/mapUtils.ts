cd8dd073afa797f2ba9b9ec303db717eef0456bd
CaryHu <cary.hu@developertools.com>
2023-06-13 17:17:36 +0800

    fix: wrong d.ts used

--- src/mapUtils.ts
+++ src/mapUtils.ts
@@ -1,11 +1,10 @@
-import { distance as _distance, point } from '@turf/turf'
+import { distance as _distance, point } from 'turf'
 
 export class MapUtils {
   public static distance(pointA: Array<number>, pointB: Array<number>) {
     const from = point(pointA)
     const to = point(pointB)
-    const options = { units: 'miles' }
 
-    return _distance(from, to, options)
+    return _distance(from, to)
   }
 }
